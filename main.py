c7f52f447ff4e498944868a05afab16d1f8a8c7d
“rashaamro” <rashaamro3@gmail.com>
2023-08-04 13:44:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+num1 = int(input("first no:"))
+num2 = int(input("second no:"))
+
+def add(num1: int, num2: int):
+    return num2 + num1
+
+def sub(num1: int, num2: int):
+    return num2 - num1
+
+
+def mul(num1: int, num2: int):
+    return num2 * num1
+
+
+def div(num1: int, num2: int):
+    return num2 / num1
+
+
+
+print(add(num1, num2))
+print(sub(num1, num2))
+print(div(num1, num2))
+print(mul(num1, num2))
+
+
